f4b3a22a993748646f50517d16de4b2318f58c90
Mia-ly-huang <1589266135@qq.com>
2026-06-17 16:56:09 +0800

    Add files via upload
    前端

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/xiaoshou-front/' : '/',
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+})
